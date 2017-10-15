ad4a2ad19d75992eaff615cfb6e760618d33665a
Aaron Dunphy <aaronjohndunphy@gmail.com>
2017-10-15 14:39:39 +0100

    Added button to access all pages

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,3 +8,19 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 </head>
 <body>
+
+<div class="container">
+	<div class="float-right">
+		<div class="dropdown">
+			<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button>
+		  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+		    <a class="dropdown-item" href="/dashboard">Dashboard</a>
+		    <a class="dropdown-item" href="/twitter">Add Twitter</a>
+		    <a class="dropdown-item" href="/instagram">Add Instagram</a>
+		    <a class="dropdown-item" href="/github">Add GitHub</a>
+		    <a class="dropdown-item" href="/logout">Logout</a>
+		  </div>
+		</div>
+	</div>
+	<div class="clearfix"></div>
+</div>
